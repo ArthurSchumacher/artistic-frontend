f77a4e5ce85412c876cb402af87460faedb521eb
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2025-06-16 13:30:32 -0400

    Day 80.5.

--- src/app/(routes)/contact/layout.tsx
+++ src/app/(routes)/contact/layout.tsx
@@ -4,7 +4,7 @@ export default function ContactLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col w-screen items-center justify-center gap-y-8 p-8 sm:p-16 mt-44">
+    <section className="flex flex-col sm:flex-row w-screen items-center sm:items-start justify-center gap-y-8 p-8 sm:p-16 mt-44">
       {children}
     </section>
   );
